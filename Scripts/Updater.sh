1c41c748a03640d8cdd6e167618bc850df4945ac
Logicye <bp.brent01@gmail.com>
2022-02-24 08:32:31 +1000

    Update Updater.sh

--- Scripts/Updater.sh
+++ Scripts/Updater.sh
@@ -2,6 +2,8 @@
 # curl https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/Complete_Install.sh --output Complete_Install.sh
 # curl https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/Updater.sh --output Updater.sh
 Version="0.2.3"
+echo "$Version"
+sleep 2
 wget -O Complete_Install.sh https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/Complete_Install.sh
 wget -O Updater.sh https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/Updater.sh
 bash Complete_Install.sh
