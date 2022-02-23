67ef223e79690e0cd4dfc15df8dbbf2a0ccbf691
Logicye <bp.brent01@gmail.com>
2022-02-23 17:46:14 +1000

    Update test.sh

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -5,4 +5,5 @@ cd ..
 cp CyberXSecurity-Project-1/Scripts/test.sh ../testing1/
 cp CyberXSecurity-Project-1/Scripts/Complete_Install.sh ../testing1/
 rm -r ELK_Stack_Install
+rm log.txt
 bash Complete_Install.sh
