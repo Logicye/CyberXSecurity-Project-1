ade588d541d5fd6ad5ab7fbd104510854c9ab933
Logicye <bp.brent01@gmail.com>
2022-02-24 03:57:12 +1000

    Update test.sh

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -6,7 +6,7 @@ cd ..
 cp CyberXSecurity-Project-1/Scripts/test.sh ../testing1/
 cp CyberXSecurity-Project-1/Scripts/Complete_Install.sh ../testing1/
 rm -r ELK_Stack_Install
-echo "wiped" > log.txt
+echo "---------------------------" > log.txt
 bash Complete_Install.sh
 LogDir=`pwd`
 if [ -f "$LogDir/log.txt" ];
