1266821725140a1f1d83873473b291b46012a7ca
Logicye <bp.brent01@gmail.com>
2022-02-23 16:43:48 +1000

    Update Complete_Install.sh

--- Scripts/Complete_Install.sh
+++ Scripts/Complete_Install.sh
@@ -2,12 +2,12 @@ clear
 mkdir ELK_Stack_Install
 cd ELK_Stack_Install
 wget --no-check-certificate --content-disposition -O Complete_Install.yml https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/Complete_Install.yml
-print "\nComplete_Install.yml Complete\n"
+printf "\nComplete_Install.yml Complete\n"
 wget --no-check-certificate --content-disposition -O filebeat-config.yml https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/FileBeat/filebeat-config.yml
-print "\nfilebeat-config.yml Complete\n"
+printf "\nfilebeat-config.yml Complete\n"
 wget --no-check-certificate --content-disposition -O metricbeat-config.yml https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/MetricBeat/metricbeat-config.yml
-print "\nmetricbeat-config.yml Complete\n"
+printf "\nmetricbeat-config.yml Complete\n"
 wget --no-check-certificate --content-disposition -O metricbeat-docker-config.yml https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/MetricBeat/metricbeat-docker-config.yml
-print "\nmetricbeat-docker-config.yml Complete\n"
-curDir = pwd
-print curDir
+printf "\nmetricbeat-docker-config.yml Complete\n"
+curDir=pwd
+printf $curDir
