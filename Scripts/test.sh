b95df0277c7d08afc69a83d3fe115311f8e6e075
Logicye <bp.brent01@gmail.com>
2022-02-24 03:39:11 +1000

    Update test.sh

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -3,7 +3,7 @@ clear
 cd CyberXSecurity-Project-1
 git pull
 cd ..
-cp CyberXSecurity-Project-1/Scripts/test.sh ../testing1/test
+cp CyberXSecurity-Project-1/Scripts/test.sh ../testing1/
 cp CyberXSecurity-Project-1/Scripts/Complete_Install.sh ../testing1/
 rm -r ELK_Stack_Install
 rm log.txt
