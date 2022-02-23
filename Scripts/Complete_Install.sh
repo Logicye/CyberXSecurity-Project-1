99fc2a8af655f487079e6490e33483c9771cdfac
Logicye <bp.brent01@gmail.com>
2022-02-23 16:51:20 +1000

    bash run update

--- Scripts/Complete_Install.sh
+++ Scripts/Complete_Install.sh
@@ -1,13 +1,15 @@
 clear
+Green='\033[0;32m'
+NoColour='\033[0m'
 mkdir ELK_Stack_Install
 cd ELK_Stack_Install
 wget --no-check-certificate --content-disposition -O Complete_Install.yml https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/Complete_Install.yml
-printf "\nComplete_Install.yml Complete\n"
+printf "${Green}Complete_Install.yml Complete${NoColour}\n\n"
 wget --no-check-certificate --content-disposition -O filebeat-config.yml https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/FileBeat/filebeat-config.yml
-printf "\nfilebeat-config.yml Complete\n"
+printf "${Green}filebeat-config.yml Complete${NoColour}\n\n"
 wget --no-check-certificate --content-disposition -O metricbeat-config.yml https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/MetricBeat/metricbeat-config.yml
-printf "\nmetricbeat-config.yml Complete\n"
+printf "${Green}metricbeat-config.yml Complete${NoColour}\n\n"
 wget --no-check-certificate --content-disposition -O metricbeat-docker-config.yml https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/MetricBeat/metricbeat-docker-config.yml
-printf "\nmetricbeat-docker-config.yml Complete\n"
-curDir=pwd
+printf "${Green}metricbeat-docker-config.yml Complete${NoColour}\n\n"
+curDir = pwd
 printf $curDir
---
+++ Scripts/test.sh
@@ -0,0 +1,3 @@
+cp CyberXSecurity-Project-1/Scripts/Complete_Install.sh ../testing1/
+rm -r ELK_Stack_Install
+bash Complete_Install.sh
