4fb978b39b23586a75c55b1d63b91b9bbfc4e145
Logicye <bp.brent01@gmail.com>
2022-02-23 17:30:43 +1000

    Update Complete_Install.sh

--- Scripts/Complete_Install.sh
+++ Scripts/Complete_Install.sh
@@ -3,6 +3,18 @@ Green='\033[0;32m'
 Blue='\033[0;34m'
 NoColour='\033[0m'
 
+# IP Addresses to replace
+ReplaceIP= ""
+printf "Replace IP Address: $ReplaceIP"
+SeacrchIP= "10.1.0.4"
+printf "Search IP Address: $SearchIP"
+
+# Directories to replace 
+ReplaceDir= pwd
+printf "Replace Directory: $ReplaceDir"
+SearchDir= "/root/CyberXSecurity-Project-1/Scripts/" # Remember to add file directory for each change in seperate files ie (metricbeat/met...)
+printf "Search Directory: $SearchDir"
+
 mkdir ELK_Stack_Install
 cd ELK_Stack_Install
 wget --no-check-certificate --content-disposition -O Complete_Install.yml https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/Complete_Install.yml
@@ -13,5 +25,6 @@ wget --no-check-certificate --content-disposition -O metricbeat-config.yml https
 printf "${Green}metricbeat-config.yml Complete${NoColour}\n\n"
 wget --no-check-certificate --content-disposition -O metricbeat-docker-config.yml https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/MetricBeat/metricbeat-docker-config.yml
 printf "${Green}metricbeat-docker-config.yml Complete${NoColour}\n\n"
-CurDir= pwd
-printf ${Blue}"$CurDir"
+#Replace
+sed "s/10.1.0.4/$ReplaceIP/g" filebeat-config.yml
+sed "s/10.1.0.4/$ReplaceIP/g" metricbeat-config.yml
