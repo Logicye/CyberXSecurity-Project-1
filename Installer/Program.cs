73a41a8a14b567c46ae8b88f6a0e553c8ad21992
Logicye <bp.brent01@gmail.com>
2022-02-24 02:06:27 +1000

    c# installer attempt

--- Installer/Program.cs
+++ Installer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Installer
 {
@@ -7,6 +8,30 @@ namespace Installer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+#if true
+            ProcessStartInfo startInfo = new ProcessStartInfo()
+            {
+                WorkingDirectory = @"C:\Users\\access\Documents\GitHub\CyberXSecurity-Project-1\Tester",
+                //FileName = "complete_install.yml",
+                FileName = "installer.exe",
+                CreateNoWindow = false,
+                UseShellExecute = false,
+                
+                Arguments = "",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                RedirectStandardOutput = true,
+            };
+            using Process process = new Process()
+            {
+                StartInfo = startInfo,
+            };
+            process.Start();
+            //string output = process.StandardOutput.ReadToEnd();
+
+            process.WaitForExit();
+            
+#endif
+            Environment.Exit(0);
         }
     }
 }
--- Scripts/Installer.cs
+++
@@ -1,3 +0,0 @@
-if(){
-    Console.WriteLine($"Hello {System.Environment.GetEnvironmentVariable("USER")}! I'm {System.Environment.MachineName} and I'm talking to you from {System.IO.Directory.GetCurrentDirectory()}");
-}
