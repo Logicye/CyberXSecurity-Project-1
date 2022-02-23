39a6616f5470f1dac47aaab50bb2cc33d36cd246
Logicye <bp.brent01@gmail.com>
2022-02-24 08:28:33 +1000

    Update Updater.sh

--- Scripts/Updater.sh
+++ Scripts/Updater.sh
@@ -4,3 +4,4 @@
 wget -O Complete_Install.sh https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/Complete_Install.sh
 wget -O Updater.sh https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/Updater.sh
 bash Complete_Install.sh
+exit
