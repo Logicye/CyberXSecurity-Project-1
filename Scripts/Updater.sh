2e5cffb3c6e8f5c108c9ad1cc62b7904be4fb824
Logicye <bp.brent01@gmail.com>
2022-02-26 00:05:05 +1000

    v0.3.8

--- Scripts/Updater.sh
+++ Scripts/Updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # curl https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/Complete_Install.sh --output Complete_Install.sh
 # curl https://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/Updater.sh --output Updater.sh
-Version="Version(Updater) - 0.3.7"
+Version="Version(Updater) - 0.3.8"
 echo "$Version"
 sleep 1
 
