9af77bb7f0d64bae0ab94eda7495fc333ecb784d
Logicye <bp.brent01@gmail.com>
2022-02-26 03:51:01 +1000

    Update Complete_Install.sh

--- Scripts/Complete_Install.sh
+++ Scripts/Complete_Install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 clear
-Version="Version - 0.3.12.20"
+Version="Version - 0.3.12.21"
 Config_Files="/etc/Elk_Install_Files"
 
 if [ $(whoami) != 'root' ]; then
@@ -189,7 +189,7 @@ Web_Server_Set() {
         if [ "$WebServerExist" == "[webservers]" ];then
                 echo "webservers does exit"
                 
-        elif 
+        elif [ "$WebServerExist" == "## [webservers]" ];then
                 echo "## webservers does exist"
 
         else
