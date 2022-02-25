1a85672d1f24bf6b0f3aa583035746c7ab5daf9b
Logicye <bp.brent01@gmail.com>
2022-02-26 00:31:40 +1000

    decomissioned updater.sh

--- Scripts/Updater(outofuse).sh
+++ Scripts/Updater(outofuse).sh
@@ -4,20 +4,6 @@
 Version="Version(Updater) - 0.3.11"
 echo "$Version"
 sleep 1
-
-# POSITIONAL_ARGS=()
-
-# while [[ $# -gt 0 ]]; do
-#     case $1 in
-#         -d|--directory)
-#             CurDir= "$2"
-#         ;;
-#         *)
-#         ;;
-#     esac
-# done
-
-sync; echo 3 > /proc/sys/vm/drop_caches 
 wget -m --no-cache --no-check-certificate -O /bin/Complete_Install http://raw.githubusercontent.com/Logicye/CyberXSecurity-Project-1/main/Scripts/Complete_Install.sh
 chmod u+x /bin/Complete_Install
 chmod 777 /bin/Complete_Install
